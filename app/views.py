e08f3344f9ad3352134fed89a922e7e01216918d
IbrahimNassoura <ibnassoura@yahoo.com>
2018-03-07 10:20:12 +0400

    views file import error

--- app/views.py
+++ app/views.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-import models
-import stores
-from app import *
-
-app = Flask(__name__)
+from flask import render_template, request, redirect, url_for
+from app import models
+from app import app, member_store, post_store
 
 @app.route("/")
 @app.route("/index")
 def home():
-	return render_template(	"index.html", posts = post_store.get_all())
+    return render_template("index.html", posts = post_store.get_all())
+
 
 @app.route("/topic/add", methods = ["GET", "POST"])
 def topic_add():
-	if request.method == "POST":
-		new_post = models.Post(request.form["title"], request.form["content"])
-		post_store.add(new_post)
-		return redirect(url_for("home"))
-	else:
-		return render_template("topic_add.html")
+    if request.method == "POST":
+        new_post = models.Post(request.form["title"], request.form["content"])
+        post_store.add(new_post)
+        return redirect(url_for("home"))
+
+    else:
+        return render_template("topic_add.html")
